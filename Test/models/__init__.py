679e91d65ab6d72726a52ec407035b608b5251a9
omargaer <omarololoshkin@gmail.com>
2024-12-12 12:18:57 +1000

    Исправил файловую структуру 2.0/

---
+++ Test/models/__init__.py
@@ -0,0 +1,12 @@
+#from application_models import *
+from .companies_models import CompanyGroup, Company, BranchOffice, BranchOfficeStatus, BranchOfficeType,  Position
+# from hard_soft_ware_models import *
+# from user_models import *
+__all__ = [
+    'CompanyGroup',
+    'Company',
+    'BranchOffice',
+    'BranchOfficeStatus',
+    'BranchOfficeType',
+    'Position',
+]

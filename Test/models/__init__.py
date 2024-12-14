5a9b9e329b8a038fe7fcca4e75c912e68fb9dd48
omargaer <omarololoshkin@gmail.com>
2024-12-15 00:12:21 +1000

    Доделал АиПО. Добавил constraint на действия по ремонту и обслуживанию.

--- Test/models/__init__.py
+++ Test/models/__init__.py
@@ -1,12 +1,15 @@
 #from application_models import *
 from .companies_models import (CompanyGroup,
+                               CompanyGroupDecisionMaker,
                                Company,
+                               CompanyDecisionMaker,
                                Position,
                                BranchOffice,
                                BranchOfficeStatus,
                                BranchOfficeType,
                                BranchOfficeLocation,
-                               BranchOfficeSchedule)
+                               BranchOfficeSchedule,
+                               AccountOfBranchEmployees)
 # from hard_soft_ware_models import *
 from .user_models import (IndividualEntity,
                           Account,
@@ -16,10 +19,13 @@ from .hard_soft_ware_models import (DeviceType,
                                     DevicePlacementMethod,
                                     Device,
                                     SoftwareType,
-                                    Software)
+                                    Software,
+                                    MaintenanceAction)
 __all__ = [
     'CompanyGroup',
+    'CompanyGroupDecisionMaker',
     'Company',
+    'CompanyDecisionMaker',
     'BranchOffice',
     'BranchOfficeStatus',
     'BranchOfficeType',
@@ -27,6 +33,16 @@ __all__ = [
     'BranchOffice',
     'BranchOfficeLocation',
     'BranchOfficeSchedule',
+    'AccountOfBranchEmployees',
     'Account',
     'IndividualEntity',
+    'Account',
+    'AccountRole',
+    'AccountStatus',
+    'SoftwareType',
+    'Software',
+    'MaintenanceAction',
+    'DeviceType',
+    'DevicePlacementMethod',
+    'Device',
 ]
--- Test/models/hard_soft_ware_models.py
+++ Test/models/hard_soft_ware_models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.db import models
+from django.db.models import Q
 
 from Test.models import BranchOfficeLocation
 
@@ -59,44 +60,55 @@ class Device(models.Model):
 
     class Meta:
         db_table = 'Device'
-        verbose_name = "Оборудование"
-        verbose_name_plural = "Оборудование"
+        verbose_name = 'Оборудование'
+        verbose_name_plural = 'Оборудование'
 
 
 class SoftwareType(models.Model):
     name = models.CharField(max_length=20,
                             null=False,
-                            verbose_name="Тип программного обеспечения")
+                            verbose_name='Тип программного обеспечения')
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'SoftwareType'
-        verbose_name = "Тип программного обеспечения"
-        verbose_name_plural = "Типы программного обеспечения"
+        verbose_name = 'Тип программного обеспечения'
+        verbose_name_plural = 'Типы программного обеспечения'
 
 class Software(models.Model):
+    device = models.ForeignKey(Device,
+                               on_delete=models.RESTRICT,
+                               null=False,
+                               default=None,
+                               related_name='software',
+                               verbose_name='Оборудование')
+    type = models.ForeignKey(SoftwareType,
+                             on_delete=models.RESTRICT,
+                             null=False,
+                             default=None,
+                             verbose_name='Тип оборудования')
     name = models.CharField(max_length=20,
                             null=False,
-                            verbose_name="Название программного обеспечения")
+                            verbose_name='Название программного обеспечения')
     price = models.DecimalField(decimal_places=2,
                                 max_digits=15,
                                 null=True,
                                 default=None,
-                                verbose_name="Закупочная цена")
+                                verbose_name='Закупочная цена')
     license_key = models.CharField(max_length=200,
                                    null=True,
                                    default=None,
-                                   verbose_name="Лицензионный ключ")
+                                   verbose_name='Лицензионный ключ')
     purchaseDate = models.DateField(null=True,
                                     default=None,
-                                    verbose_name="Дата покупки")
+                                    verbose_name='Дата покупки')
     licenseActivationDate = models.DateField(null=True,
                                              default=None,
-                                             verbose_name="Дата активации лицензии")
+                                             verbose_name='Дата активации лицензии')
     licenseExpirationDate = models.DateField(null=True,
                                              default=None,
-                                             verbose_name="Дата окончания лицензии")
+                                             verbose_name='Дата окончания лицензии')
     def __str__(self):
         return self.name
 
@@ -104,3 +116,107 @@ class Software(models.Model):
         db_table = 'Software'
         verbose_name = "Программное обеспечение"
         verbose_name_plural = "Программное обеспечение"
+
+class HardwareType(models.Model):
+    name = models.CharField(max_length=20,
+                            null=False,
+                            verbose_name="Тип аппаратного обеспечения")
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'HardwareType'
+        verbose_name = 'Тип аппаратного обеспечения'
+        verbose_name_plural = 'Типы аппаратного обеспечения'
+
+class Hardware(models.Model):
+    device = models.ForeignKey(Device,
+                               on_delete=models.RESTRICT,
+                               null=True,
+                               default=None,
+                               related_name='hardware',
+                               verbose_name='Оборудование')
+    type = models.ForeignKey(HardwareType,
+                             on_delete=models.RESTRICT,
+                             null=False,
+                             verbose_name='Тип аппаратного обеспечения')
+    modelName = models.CharField(max_length=30,
+                                 null=True,
+                                 default='',
+                                 verbose_name="Модель")
+    price = models.DecimalField(decimal_places=2,
+                                max_digits=15,
+                                null=True,
+                                default=None,
+                                verbose_name='Закупочная цена')
+    inventoryNumber = models.CharField(max_length=20,
+                                       null=True,
+                                       default=None,
+                                       verbose_name="Инвентарный номер")
+    factoryNumber = models.CharField(max_length=20,
+                                     null=True,
+                                     default=None,
+                                     verbose_name="Заводской номер")
+    purchaseDate = models.DateField(null=True,
+                                    default=None,
+                                    verbose_name='Дата покупки')
+    installationDate = models.DateField(null=True,
+                                        default=None,
+                                        verbose_name='Дата установки')
+    warrantyExpirationDate = models.DateField(null=True,
+                                              default=None,
+                                              verbose_name='Дата окончания гарантии')
+    def __str__(self):
+        return self.type.name + " " + self.modelName
+
+    class Meta:
+        db_table = 'Hardware'
+        verbose_name = 'Аппаратное обеспечение'
+        verbose_name_plural = 'Аппаратное обеспечение'
+
+class MaintenanceAction(models.Model):
+    device = models.ForeignKey(Device,
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True,
+                               related_name="maintenance_actions",
+                               verbose_name="Оборудование")
+    hardware = models.ForeignKey(Hardware,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True,
+                                 related_name="maintenance_actions",
+                                 verbose_name="Аппаратное обеспечение")
+    action_type = models.CharField(max_length=100,
+                                   null=False,
+                                   default=None,
+                                   verbose_name="Тип действия")
+    description = models.TextField(null=True,
+                                   blank=True,
+                                   verbose_name="Описание действия")
+    action_date = models.DateTimeField(auto_now_add=True,
+                                       verbose_name="Дата выполнения действия")
+    contractor = models.ForeignKey('IndividualEntity',
+                                   on_delete=models.RESTRICT,
+                                   null=True,
+                                   verbose_name="Исполнитель",
+                                   related_name="maintenance_actions")
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(
+                        Q(device__isnull=False, hardware__isnull=True) |
+                        Q(device__isnull=True, hardware__isnull=False)
+                ),
+                name="device_or_hardware_only"
+            )
+        ]
+        db_table = 'MaintenanceAction'
+        verbose_name = 'Действие по ремонту и обслуживанию'
+        verbose_name_plural = 'Действия по ремонту и обслуживанию'
+
+
+    def __str__(self):
+        target = self.device or self.hardware
+        return f"{self.action_type} - {target}"
